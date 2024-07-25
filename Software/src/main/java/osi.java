0b8e191c2d717291f05715d12635641af170ca7e
Omar Khatib <Omar Khatib@10.0.0.8>
2024-07-25 20:25:25 +0300

    222

--- Software/src/main/java/osi.java
+++ Software/src/main/java/osi.java
@@ -29,7 +29,7 @@ public class osi extends JFrame {
 	 */
 	public osi() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 851, 664);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
