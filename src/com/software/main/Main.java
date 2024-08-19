d7773db1be0c3fa038d6ba8d787b04fa14233617
XbetaApps <142219848+XbetaApps@users.noreply.github.com>
2024-08-19 15:21:26 +0300

    Create Main.java

---
+++ src/com/software/main/Main.java
@@ -0,0 +1,14 @@
+package com.software.main;
+
+import com.software.ui.WelcomeFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new WelcomeFrame().setVisible(true);
+            }
+        });
+    }
+}
