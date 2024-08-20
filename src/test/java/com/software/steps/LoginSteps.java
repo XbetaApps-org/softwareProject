fa48f667f0e80573f2196c13bdc303a100fb0f26
XbetaApps <142219848+XbetaApps@users.noreply.github.com>
2024-08-20 12:29:08 +0300

    Update LoginSteps.java

--- src/test/java/com/software/steps/LoginSteps.java
+++ src/test/java/com/software/steps/LoginSteps.java
@@ -1,8 +1,4 @@
-package com.software.steps;
-
 import static org.junit.Assert.assertEquals;
-
-import java.awt.event.ActionEvent;
 
 import com.software.ui.LoginFrame;
 
@@ -19,6 +15,7 @@ public class LoginSteps {
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);  // Make sure the frame is visible
     }
 
     @And("the user selects login as {string} from a combo box")
@@ -34,11 +31,18 @@ public class LoginSteps {
 
     @And("clicks on the login button")
     public void clicks_on_the_login_button() {
-        loginFrame.loginButtonActionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+        // Simulate the user clicking the login button
+        loginFrame.getLoginButton().doClick();
     }
 
     @Then("the user will see a message {string}")
     public void the_user_will_see_a_message(String expectedMessage) {
+        // This step may need to wait a moment for the GUI thread to process
+        try {
+            Thread.sleep(500); // Adjust as necessary for your environment
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         message = loginFrame.getDisplayedMessage();
         assertEquals(expectedMessage, message);
     }
