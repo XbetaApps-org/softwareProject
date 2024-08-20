071678a7ad74818fa4ca595c2541ea3117937317
XbetaApps <142219848+XbetaApps@users.noreply.github.com>
2024-08-20 11:31:03 +0300

    Update LoginSteps.java

--- src/test/java/com/software/steps/LoginSteps.java
+++ src/test/java/com/software/steps/LoginSteps.java
@@ -4,10 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.awt.event.ActionEvent;
 
-import javax.swing.JComboBox;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
 import com.software.ui.LoginFrame;
 
 import io.cucumber.java.en.And;
@@ -18,28 +14,22 @@ import io.cucumber.java.en.When;
 public class LoginSteps {
 
     private LoginFrame loginFrame;
-    private JTextField userNameField;
-    private JPasswordField passwordField;
-    private JComboBox<String> roleComboBox;
     private String message;
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         loginFrame = new LoginFrame();
-        userNameField = loginFrame.userNameField;
-        passwordField = loginFrame.PasswordField;
-        roleComboBox = loginFrame.jComboBox1;
     }
 
     @And("the user selects login as {string} from a combo box")
     public void the_user_selects_login_as_from_a_combo_box(String role) {
-        roleComboBox.setSelectedItem(role);
+        loginFrame.setUserType(role);
     }
 
     @When("the user enters username {string} and password {string}")
     public void the_user_enters_username_and_password(String username, String password) {
-        userNameField.setText(username);
-        passwordField.setText(password);
+        loginFrame.setUserNameField(username);
+        loginFrame.setPasswordField(password);
     }
 
     @And("clicks on the login button")
@@ -49,7 +39,6 @@ public class LoginSteps {
 
     @Then("the user will see a message {string}")
     public void the_user_will_see_a_message(String expectedMessage) {
-        // Simulate showing a dialog and capture the message
         message = loginFrame.getDisplayedMessage();
         assertEquals(expectedMessage, message);
     }
