63e07cf8332ec5d387b1a16ca5d2fca24cccea06
XbetaApps <142219848+XbetaApps@users.noreply.github.com>
2024-08-20 12:38:10 +0300

    Update WelcomePageSteps.java

--- src/test/java/com/software/steps/WelcomePageSteps.java
+++ src/test/java/com/software/steps/WelcomePageSteps.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.*;
 import static org.junit.Assert.*;
 
 public class WelcomePageSteps {
+
     private WelcomeFrame welcomeFrame;
     private SignUpFrame signUpFrame;
     private LoginFrame loginFrame;
@@ -28,39 +29,59 @@ public class WelcomePageSteps {
             case "Login":
                 isVisible = welcomeFrame.getLoginButton() != null && welcomeFrame.getLoginButton().isEnabled();
                 break;
+            default:
+                fail("Unexpected button name: " + buttonName);
+                break;
         }
         assertTrue("The " + buttonName + " button should be visible.", isVisible);
     }
 
     @When("the user clicks the {string} button")
     public void the_user_clicks_the_button(String buttonName) {
-    	 boolean isVisible = false;
-         switch (buttonName) {
-             case "Sign Up":
-                 isVisible = welcomeFrame.getSignUpButton() != null && welcomeFrame.getSignUpButton().isEnabled();
-                 break;
-             case "Login":
-                 isVisible = welcomeFrame.getLoginButton() != null && welcomeFrame.getLoginButton().isEnabled();
-                 break;
-         }
-         assertTrue("The " + buttonName + " button should be visible.", isVisible);
-        
+        boolean isVisible = false;
+        switch (buttonName) {
+            case "Sign Up":
+                isVisible = welcomeFrame.getSignUpButton() != null && welcomeFrame.getSignUpButton().isEnabled();
+                break;
+            case "Login":
+                isVisible = welcomeFrame.getLoginButton() != null && welcomeFrame.getLoginButton().isEnabled();
+                break;
+            default:
+                fail("Unexpected button name: " + buttonName);
+                break;
         }
-    
+        assertTrue("The " + buttonName + " button should be visible.", isVisible);
+
+        // Perform the click action if the button is visible
+        if (isVisible) {
+            switch (buttonName) {
+                case "Sign Up":
+                    welcomeFrame.getSignUpButton().doClick();
+                    break;
+                case "Login":
+                    welcomeFrame.getLoginButton().doClick();
+                    break;
+                default:
+                    fail("Unexpected button name for clicking: " + buttonName);
+                    break;
+            }
+        }
+    }
 
     @Then("the user should see a {string} page")
     public void the_user_should_see_a_page(String pageName) {
-    	 boolean isVisible = false;
-         switch (pageName) {
-             case "Sign Up":
-                 isVisible = welcomeFrame.getSignUpButton() != null && welcomeFrame.getSignUpButton().isEnabled();
-                 break;
-             case "Login":
-                 isVisible = welcomeFrame.getLoginButton() != null && welcomeFrame.getLoginButton().isEnabled();
-                 break;
-         }
-         assertTrue("The " + pageName + " button should be visible.", isVisible);
-        
-       
+        boolean isVisible = false;
+        switch (pageName) {
+            case "Sign Up":
+                isVisible = welcomeFrame.getSignUpButton() != null && welcomeFrame.getSignUpButton().isEnabled();
+                break;
+            case "Login":
+                isVisible = welcomeFrame.getLoginButton() != null && welcomeFrame.getLoginButton().isEnabled();
+                break;
+            default:
+                fail("Unexpected page name: " + pageName);
+                break;
+        }
+        assertTrue("The " + pageName + " page should be visible.", isVisible);
     }
 }
